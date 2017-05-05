712860924fe06810c5d176e4fb73e66afac628f8
Nasser Benoudi <n.benoudi@gmail.com>
2017-05-05 11:54:03 +0200

    First Commit

---
+++ Project_test/src/Nas/Test_Code/CL_GGT.java
@@ -0,0 +1,28 @@
+package Nas.Test_Code;
+
+public class CL_GGT {
+	private int GGT=0;
+	
+
+	public CL_GGT(int gGT) {
+		super();
+		GGT = gGT;
+	}
+
+
+	public int getGGT() {
+		return GGT;
+	}
+
+
+	public void setGGT(int gGT) {
+		GGT = gGT;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
